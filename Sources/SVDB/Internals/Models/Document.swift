434fdd51609637f5a7e59ad27610316488c4614c
johnbean393 <johnbean393@gmail.com>
2024-10-04 22:02:39 +0800

    Added convenient initializer

--- Sources/SVDB/Internals/Models/Document.swift
+++ Sources/SVDB/Internals/Models/Document.swift
@@ -6,8 +6,10 @@
 //
 
 import Foundation
+import NaturalLanguage
 
 public struct Document: Codable, Identifiable {
+	
     public let id: UUID
     public let text: String
     public let embedding: [Double]
@@ -19,4 +21,30 @@ public struct Document: Codable, Identifiable {
         self.embedding = embedding
         self.magnitude = sqrt(embedding.reduce(0) { $0 + $1 * $1 })
     }
+	
+	public init(text: String) throws {
+		self.id = UUID()
+		self.text = text
+		// Identify text language
+		let recognizer: NLLanguageRecognizer = NLLanguageRecognizer()
+		recognizer.processString(text)
+		let language: NLLanguage = recognizer.dominantLanguage ?? .english
+		// Get embedding
+		guard let embeddingModel: NLEmbedding = NLEmbedding.sentenceEmbedding(
+			for: language
+		) else {
+			throw EmbeddingError.failedToInitEmbeddingModel
+		}
+		guard let embedding = embeddingModel.vector(for: text) else {
+			throw EmbeddingError.failedToGetEmbedding
+		}
+		self.embedding = embedding
+		self.magnitude = sqrt(embedding.reduce(0) { $0 + $1 * $1 })
+	}
+	
+	public enum EmbeddingError: Error {
+		case failedToInitEmbeddingModel
+		case failedToGetEmbedding
+	}
+	
 }
